f59986323c7b7323b609a5f72ee135d6a0719b96
Arthur Marinheiro Vilela <av@westron.media>
2021-08-29 12:58:06 -0300

    Adicionado commando para deleter mensagens

--- plataform/commands/deleteMsg.go
+++ plataform/commands/deleteMsg.go
@@ -3,6 +3,8 @@ package commands
 import (
 	"github.com/bwmarrin/discordgo"
 	"mybot/plataform/router"
+	"strconv"
+	"strings"
 )
 
 var DeleteMsgCmd = router.Command{
@@ -12,5 +14,54 @@ var DeleteMsgCmd = router.Command{
 }
 
 func deleteMsg(s *discordgo.Session, m *discordgo.MessageCreate) error {
-	return nil
+	args := strings.Fields(m.Content)
+
+	if len(args) < 2 {
+		_, err := s.ChannelMessageSendReply(
+			m.Message.ChannelID,
+			"Argumentos inválido. Use %deleteMsg help para ajuda.",
+			m.Reference())
+		return err
+	}
+
+	if args[1] == "help" {
+		helpEmbed := discordgo.MessageEmbed{
+			Title:       "Delete Mensagens",
+			Description: "deleta mensagens em um canal.\nvantepaco [quantidade] [autor]\n",
+			Color:       0x009933,
+			Fields: []*discordgo.MessageEmbedField{
+				&discordgo.MessageEmbedField{
+					Name:   "quantidade",
+					Value:  "Quantidade de mensagens a serem deletada, mínimo 1 e máximo 100",
+					Inline: false,
+				},
+				&discordgo.MessageEmbedField{
+					Name:   "autor",
+					Value:  "Autor das mensagens a serem deletadas. Opcional",
+					Inline: false,
+				},
+			},
+		}
+		_, err := s.ChannelMessageSendEmbed(m.ChannelID, &helpEmbed)
+		return err
+	}
+
+	amount, err := strconv.ParseInt(args[1], 10, 32)
+	if err != nil || (amount < 1 || amount > 100) {
+		_, err := s.ChannelMessageSendReply(
+			m.Message.ChannelID,
+			"Argumentos inválido. Use %deleteMsg help para ajuda.",
+			m.Reference())
+		return err
+	}
+
+	messages, _ := s.ChannelMessages(m.ChannelID, int(amount), "", "", "")
+	var messagesId []string
+	for _, m := range messages {
+		messagesId = append(messagesId, m.ID)
+	}
+
+	err = s.ChannelMessagesBulkDelete(m.ChannelID, messagesId)
+
+	return err
 }
