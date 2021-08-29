942bcb4613364c605f99c5aa9e3ea70f72ae64b1
Arthur Marinheiro Vilela <av@westron.media>
2021-08-28 23:35:42 -0300

    Adicionados modos (sfw | nsfw) vampeta

--- plataform/commands/vamps.go
+++ plataform/commands/vamps.go
@@ -7,6 +7,7 @@ import (
 	"mybot/plataform/router"
 	"strconv"
 	"strings"
+	"time"
 )
 
 var VampetacoCmd = router.Command{
@@ -23,24 +24,49 @@ var vampsPicsSFW = []string{
 	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXr_AGxe25hOG1_mJukMfSwwLPIOXxlqZpLHu01Y9S6Zl2vgTXlHVQMJi4Yng6CTUguAo&usqp=CAU",
 	"https://f.i.uol.com.br/fotografia/2013/10/31/331934-970x600-1.jpeg",
 	"https://scontent.fcgh17-1.fna.fbcdn.net/v/t1.18169-9/22308819_1895786947404296_892622575886006285_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=730e14&_nc_ohc=uuooey9kLLoAX_mk0kP&_nc_ht=scontent.fcgh17-1.fna&oh=1be48d3788bbf1b754608492031e1985&oe=614FBA02",
+	"https://frutilau.files.wordpress.com/2011/01/vampeta1.jpg?w=584",
+	"https://pbs.twimg.com/media/EckbF9OXoAEh6zs.jpg",
+}
+
+var vampsPicsNSFW = []string{
+	"https://alvarocantor.files.wordpress.com/2010/07/vampeta.jpg",
+	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMBFNhcvzpd4bV82ee0LqEFyPBxpu5eWzxQNBlV32obPfHmg9EdR6EhChV4vI_8zyzjr4&usqp=CAU",
+	"https://pbs.twimg.com/media/CrNcxpZWIAE3QP8.jpg",
+	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6zS8WIoVlFS78Sfx_c0WQIhX8brGnn2dWuLP7llvR47jNjtTwvDLRK9yfa-DsRGChljA&usqp=CAU",
+	"https://pbs.twimg.com/media/EV6Sm8AXgAE8WLo.jpg",
+	"https://pbs.twimg.com/media/EctO1AVWoAcPpwt.jpg",
+	"https://pbs.twimg.com/media/EWk33IhXkAInuEk.jpg",
 }
 
 func vampetaco(s *discordgo.Session, m *discordgo.MessageCreate) {
 	args := strings.Fields(m.Content)
-	amount, err := strconv.ParseInt(args[1], 10, 32)
+	mode := strings.ToLower(args[1])
+	if mode != "sfw" && mode != "nsfw" {
+		mode = "sfw"
+	}
+
+	amount, err := strconv.ParseInt(args[2], 10, 32)
 	if err != nil {
 		return
 	}
 
+	var pics []string
+
+	if mode == "sfw" {
+		pics = vampsPicsSFW
+	} else {
+		pics = vampsPicsNSFW
+	}
+
 	for i := 0; i < int(amount); i++ {
-		index := rand.Intn(len(vampsPicsSFW))
-		picUrl := vampsPicsSFW[index]
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		index := r.Intn(len(pics) - 1)
+		picUrl := pics[index]
 
 		embeded := discordgo.MessageEmbed{
-			Title:       "Vampeta",
-			Description: "vampeta",
-			Color:       0,
-			Image:       &discordgo.MessageEmbedImage{URL: picUrl},
+			Title: "Vampeta",
+			Color: 0x009933,
+			Image: &discordgo.MessageEmbedImage{URL: picUrl},
 		}
 
 		_, err := s.ChannelMessageSendEmbed(m.ChannelID, &embeded)
